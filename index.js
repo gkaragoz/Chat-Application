4eadd50d1b225a40555d0b7a1c13cbc0f883c1d4
Gökhan Karagöz <annotto3@gmail.com>
2017-02-15 00:26:36 +0300

    Broadcasting

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ io.on('connection', function(socket){
   console.log('a user connected');
 
   socket.on('chat message', function(msg){
-    console.log('message: ' + msg);
+    io.emit('chat message', msg);
   });
 
   socket.on('disconnect', function(){
