de74370314010c6985612a16cf8689f45c3f2949
Gökhan Karagöz <annotto3@gmail.com>
2017-02-15 00:23:26 +0300

    Emitting events.

--- index.js
+++ index.js
@@ -9,6 +9,10 @@ app.get('/', function(req, res){
 io.on('connection', function(socket){
   console.log('a user connected');
 
+  socket.on('chat message', function(msg){
+    console.log('message: ' + msg);
+  });
+
   socket.on('disconnect', function(){
     console.log('user disconnected');
   });
