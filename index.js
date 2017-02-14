99399b0c886533bf59489c3972bc93e1eb0b118d
Gökhan Karagöz <annotto3@gmail.com>
2017-02-15 00:14:39 +0300

    Serving HTML.

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var app = require('express')();
 var http = require('http').Server(app);
 
 app.get('/', function(req, res){
-  res.send('<h1>Hello world</h1>');
+  res.sendFile(__dirname + '/index.html');
 });
 
 http.listen(3000, function(){
