7725f1b50d549ee7830b2980737cdcc55304d8fe
Gökhan Karagöz <annotto3@gmail.com>
2017-02-15 00:20:36 +0300

    Add disconnect event.

--- index.js
+++ index.js
@@ -8,6 +8,10 @@ app.get('/', function(req, res){
 
 io.on('connection', function(socket){
   console.log('a user connected');
+
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
+  });
 });
 
 http.listen(3000, function(){
