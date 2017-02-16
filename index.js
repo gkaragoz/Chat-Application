8c43949e102d993d58b15fc704145882996cd956
Gökhan Karagöz <annotto3@gmail.com>
2017-02-16 04:37:18 +0300

    Add total clients count on server.

--- index.js
+++ index.js
@@ -2,22 +2,29 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
-app.get('/', function(req, res){
+var clientCount = 0;
+
+app.get('/', function (req, res) {
   res.sendFile(__dirname + '/index.html');
 });
 
-io.on('connection', function(socket){
+io.on('connection', function (socket) {
   console.log('a user connected');
+  clientCount++;
+  console.log('total online users: ' + clientCount);
+  io.emit('request onlines', clientCount);
 
-  socket.on('chat message', function(msg){
+  socket.on('chat message', function (msg) {
     io.emit('chat message', msg);
   });
 
-  socket.on('disconnect', function(){
+  socket.on('disconnect', function () {
     console.log('user disconnected');
+    clientCount--;
+    console.log('total online users: ' + clientCount)
   });
 });
 
-http.listen(3000, function(){
+http.listen(3000, function () {
   console.log('listening on *:3000');
 });
