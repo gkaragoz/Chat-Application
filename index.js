c70df0aed55a4f06bd3b366c400485e1a51b5e28
gkaragoz <annotto3@gmail.com>
2017-03-10 01:09:37 +0300

    Create server side codes. Basic connections and disconnect handles with socket.io

---
+++ index.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var app = express();
+var server = require('http').createServer(app);
+var io = require('socket.io').listen(server);
+
+var users = [];
+var connections = [];
+
+server.listen(process.env.PORT || 3000);
+console.log('Server started at PORT: *3000');
+
+app.get('/', function(req, res){
+    res.sendFile(__dirname + '/index.html');
+});
+
+io.sockets.on('connection', function(socket){
+    console.log('Someone is connected.');
+    connections.push(socket);
+    console.log('Total connections: ' + connections.length);
+
+    socket.on('disconnect', function(data){
+        console.log('Someone is disconnected.');
+        connections.splice(connections.indexOf(socket), 1);
+        console.log('Total connections: ' + connections.length);
+    });
+});
