28c15ac1eb42ac2caec9896148a71a6d2d731b0a
gkaragoz <annotto3@gmail.com>
2017-03-10 02:30:39 +0300

    Version 2 finished. Ready for publish.

--- index.js
+++ index.js
@@ -18,13 +18,30 @@ io.sockets.on('connection', function(socket){
     connections.push(socket);
     console.log('Total connections: ' + connections.length);
 
-    socket.on('send message', function(data){
-        io.sockets.emit('new message', {msg: data});
-    });
-
     socket.on('disconnect', function(data){
+        users.splice(users.indexOf(socket.username), 1);
+        updateUsernames();
         console.log('Someone is disconnected.');
         connections.splice(connections.indexOf(socket), 1);
         console.log('Total connections: ' + connections.length);
     });
+
+    socket.on('send message', function(data){
+        io.sockets.emit('new message', {
+            msg: data,
+            user: socket.username
+        });
+    });
+
+    socket.on('new user', function(data, callback){
+        console.log('Username received: ' + data);
+        callback(true);
+        socket.username = data;
+        users.push(socket.username);
+        updateUsernames();
+    });
+
+    function updateUsernames(){
+        io.sockets.emit('get users', users);
+    }
 });
